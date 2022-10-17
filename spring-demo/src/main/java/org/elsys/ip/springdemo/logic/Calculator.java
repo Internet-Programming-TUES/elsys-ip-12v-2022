eed57d25376629e20ff735632824a388c09c864a
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-10-17 17:31:39 +0300

    Spring Demo

---
+++ spring-demo/src/main/java/org/elsys/ip/springdemo/logic/Calculator.java
@@ -0,0 +1,7 @@
+package org.elsys.ip.springdemo.logic;
+
+public class Calculator {
+    public int aplusb(int a, int b) {
+        return a+b;
+    }
+}
---
+++ spring-demo/src/main/java/org/elsys/ip/springdemo/logic/Logic.java
@@ -0,0 +1,20 @@
+package org.elsys.ip.springdemo.logic;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Logic {
+    private final Calculator calculator;
+
+    public Logic(Calculator calculator) {
+        this.calculator = calculator;
+        System.out.println("Logic is created");
+    }
+
+    @PostConstruct
+    public void logic() {
+        System.out.println("5 + 4 = " + calculator.aplusb(5,4));
+    }
+}
