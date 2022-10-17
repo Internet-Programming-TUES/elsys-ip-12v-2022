5b0de4493ec7fe577fbc47421dbd8d490ca16426
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-10-17 17:51:13 +0300

    Servlet bootstrap

--- spring-demo/src/main/java/org/elsys/ip/springdemo/config/Config.java
+++ spring-demo/src/main/java/org/elsys/ip/springdemo/config/Config.java
@@ -1,6 +1,8 @@
 package org.elsys.ip.springdemo.config;
 
 import org.elsys.ip.springdemo.logic.Calculator;
+import org.elsys.ip.springdemo.web.CustomServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +11,13 @@ public class Config {
     @Bean
     public Calculator calculator() {
         return new Calculator();
+    }
+
+    @Bean
+    public ServletRegistrationBean exampleServletBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(
+                new CustomServlet(), "/servlet/*");
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 }
