8a7f11d3b34dc4008078aa234a749d82d031392b
Ivaylo Mihaylov <ivaylo.mihaylov@botronsoft.com>
2022-12-05 16:57:51 +0200

    Form

--- mvc/src/main/java/org/elsys/ip/mvc/GreetingController.java
+++ mvc/src/main/java/org/elsys/ip/mvc/GreetingController.java
@@ -3,7 +3,9 @@ package org.elsys.ip.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Controller
 public class GreetingController {
@@ -12,6 +14,16 @@ public class GreetingController {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
+	}
+
+	@PostMapping("/validate")
+	public String validate(
+			@RequestParam(name="firstName", required=true) String firstName,
+			@RequestParam(name="lastName", required=true) String lastName,
+			Model model) {
+		model.addAttribute("firstName", firstName);
+		model.addAttribute("lastName", lastName);
+		return "validate";
 	}
 
 }
